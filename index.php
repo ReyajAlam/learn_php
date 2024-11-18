205e0c9b8109068d70bf16c5741ea782c725948d
ReyajAlam <103191596+ReyajAlam@users.noreply.github.com>
2024-11-18 19:37:32 +0000

    learning

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+include ("database.php");
+// echo "i love this website <br>";
+// echo " and your development website <br>";
+// echo "who is next";
 
+
+$user = "aeraj";
+$password = "done";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (user, password)
+             VALUES ('$user','$hash')";
+
+try{
+    mysqli_query($conn, $sql);
+    echo "New record created successfully";
+}catch(mysqli_sql_exception){
+    echo "New record not created successfully";
+}
+// mysqli_query($conn, $sql);
+mysqli_close($conn);
+
+?>
